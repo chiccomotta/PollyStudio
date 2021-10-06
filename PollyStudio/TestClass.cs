994f80a0c0115809057271e372fb7f9506fc992a
Cristiano Motta <cristiano.motta@hotmail.it>
2021-10-06 15:50:49 +0200

    Added task exception example

---
+++ PollyStudio/TestClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace PollyStudio
+{
+    internal class TestClass
+    {
+        public static void TestTaskException()
+        {
+            var task1 = Task.Run(() => { throw new CustomException("This exception is expected!"); });
+
+            try
+            {
+                task1.Wait();
+                Console.ReadLine();
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var e in ae.InnerExceptions)
+                {
+                    // Handle the custom exception.
+                    if (e is CustomException)
+                        Console.WriteLine(e.Message);
+
+                    // Rethrow any other exception.
+                    else
+                        throw e;
+                }
+            }
+        }
+    }
+
+    public class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+        }
+    }
+}
